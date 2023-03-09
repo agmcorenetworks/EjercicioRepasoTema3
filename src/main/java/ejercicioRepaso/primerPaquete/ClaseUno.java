4a5fcb1f3dd666e9af435af4dc11571c2125d9a7
Aleexm7 <alejandromunozdominguez@gmail.com>
2023-03-09 19:37:16 +0100

    update new changes

--- src/main/java/ejercicioRepaso/primerPaquete/ClaseUno.java
+++ src/main/java/ejercicioRepaso/primerPaquete/ClaseUno.java
@@ -1,5 +1,16 @@
 package ejercicioRepaso.primerPaquete;
 
+import java.util.Random;
+
 public class ClaseUno {
 
+	
+	public static int numAleatorios() {
+		Random aleatorio = new Random();
+		
+		int generador = aleatorio.nextInt(100) +1;
+		return generador;
+	}
+	
+	
 }
